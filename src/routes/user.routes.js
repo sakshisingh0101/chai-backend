87af9d733a3a005168692accd2a67c4d932ade58
sakshisingh0101 <sakshiaingh0101>
2024-07-25 04:21:12 +0530

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express"
-import { loginUser, logoutuser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, 
+    getCurrentUser,
+    getUserChannelUpdate,
+    getUserWatchHistory,
+    loginUser,
+    logoutuser,
+    refreshAccessToken,
+    registerUser,
+    updateAccount,
+    updateAvtar,
+    updatecoverImage } 
+    from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const userRouter=Router();
@@ -22,4 +33,13 @@ userRouter.route("/login").post(loginUser)
 ///secure routes
 userRouter.route("/logout").post(verifyJWT,logoutuser)
 userRouter.route("/refresh_token").post(refreshAccessToken)
+userRouter.route("/change-password").post(verifyJWT,changeCurrentPassword)
+userRouter.route("/get-current-user").get(verifyJWT,getCurrentUser)
+userRouter.route("/update-account").patch(verifyJWT,updateAccount)
+userRouter.route("/update-avtar").patch(verifyJWT,upload.fields("avtar"),updateAvtar)
+userRouter.route("/update-coverImage").patch(verifyJWT,upload.single("coverImage"),updatecoverImage)
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelUpdate)
+userRouter.route("/watch-History").post(verifyJWT,getUserWatchHistory)
+
+
 export default userRouter
